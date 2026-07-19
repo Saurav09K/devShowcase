bb05179ddc5af2069cf15586b4e19c7c563b41a1
Saurav Kaintura <sauravkaintura49041@gmail.com>
2026-07-19 17:31:13 +0530

    Dockerize  video upload and thumbnail generation

--- backend/src/lib/generateThumbnail.js
+++ backend/src/lib/generateThumbnail.js
@@ -1,20 +1,26 @@
 const ffmpeg = require("fluent-ffmpeg");
 const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
 const path = require("path");
+const fs = require("fs");
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 const generateThumbnail = (videoPath) => {
   return new Promise((resolve, reject) => {
     const ext = path.extname(videoPath);
-
     const fileName = path.basename(videoPath, ext);
+
+    const thumbnailDir = path.join(process.cwd(), "thumbnails");
+
+    if (!fs.existsSync(thumbnailDir)) {
+      fs.mkdirSync(thumbnailDir, { recursive: true });
+    }
 
     ffmpeg(videoPath)
       .screenshots({
         timestamps: ["1"],
         filename: `${fileName}.jpg`,
-        folder: path.join(process.cwd(), "thumbnails"),
+        folder: thumbnailDir, 
         size: "320x240",
       })
       .on("end", () => {
